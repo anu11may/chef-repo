109d7cc60b8eb406e622d09abef815947ef5bdea
asingla <anupam.singla@gmail.com>
2015-04-03 15:31:10 -0500

    Cookie LWRP

---
+++ cookbooks/cookies/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: cookies
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+search('cookies_ingredients','*:*').each do |cookie_type|
+	cookies_cookie cookie_type['id'] do
+		ingredients cookie_type['ingredients']
+		bake_time 10
+		temperature 350
+		action :create
+	end
+end
---
+++ cookbooks/cookies/resources/cookie.rb
@@ -0,0 +1,6 @@
+actions :create,:eat 
+
+attribute :name, :name_attribue=> true
+attribute :bake_time :kind_of=>Integer
+attribute :temperature :kind_of=>Integer
+attribute :ingredients :kind_of=>Array
