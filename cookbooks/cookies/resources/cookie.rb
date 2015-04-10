fda52f867d2ffd83f2ee331031c84947dd705736
asingla <anupam.singla@gmail.com>
2015-04-10 11:02:33 -0500

    updated resource

--- cookbooks/cookies/resources/cookie.rb
+++ cookbooks/cookies/resources/cookie.rb
@@ -1,6 +1,6 @@
 actions :create,:eat 
 
-attribute :name, :name_attribue=> true
-attribute :bake_time :kind_of=>Integer
-attribute :temperature :kind_of=>Integer
-attribute :ingredients :kind_of=>Array
+attribute :name, :kind_of=>String
+attribute :bake_time, :kind_of=>Integer
+attribute :temperature, :kind_of=>Integer
+attribute :ingredients, :kind_of=>Array
