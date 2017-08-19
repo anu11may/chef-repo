7ca7f95638ea9df4688f8f984681ae2f1d4e129e
asingla <anupam.singla@gmail.com>
2017-08-19 11:16:17 -0500

    Changed Author cookies cookbook

--- cookbooks/cookies/metadata.rb
+++ cookbooks/cookies/metadata.rb
@@ -1,8 +1,8 @@
 name             'cookies'
-maintainer       'The Authors'
+maintainer       'Anupam Singla'
 maintainer_email 'you@example.com'
 license          'all_rights'
 description      'Installs/Configures cookies'
 long_description 'Installs/Configures cookies'
-version          '0.1.0'
+version          '0.1.1'
 
